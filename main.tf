4e435c8b49031193df586e8ccabe2b0201e3a2b3
userazure12 <91203797+userazure12@users.noreply.github.com>
2021-10-23 20:59:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,16 +2,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
-variable "key_name" {default="linuxserverrr"}
-resource "tls_private_key" "example" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-
 resource "aws_instance" "Demo" {
   ami           = "ami-09e67e426f25ce0d7"
   instance_type = "t2.micro"
   key_name = "linuxserverrr"
   tags = {
-    Name = "always-testing"
+    Name = "testbranch"
   }
 }
