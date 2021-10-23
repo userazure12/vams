489680e4573663702436076a6446f407030701b6
userazure12 <91203797+userazure12@users.noreply.github.com>
2021-10-23 21:41:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,21 +2,6 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_vpc" "automatedproject-vpc" {
-  cidr_block = "10.0.0.0/16"
-  tag = {
-      Name = "test-vpc"
-  }
-}
-
-resource "aws_subnet" "automatedproject-subnet" {
-    vpc_id = aws_vpc.automatedproject-vpc.id
-    cidr_block = "10.0.10.0/24"
-    tag = {
-          Name = "test-subnet"
-    } 
-}
-
 resource "aws_instance" automatedproject-vm" {
   ami           = "ami-074cce78125f09d61"
   instance_type = "t2.micro"
