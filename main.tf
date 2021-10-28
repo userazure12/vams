0cfd0536fe8bd3551770cefa272480eca901ba73
userazure12 <kirakkaya@protonmail.com>
2021-10-28 17:31:11 +0530

    intial commit

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "us-east-2"
+  access_key = "AKIAY6K553HHG2QPZK7O"
+  secret_key = "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"
+}
+ 
+resource "aws_instance" automatedproject-vm" {
+  ami           = "ami-074cce78125f09d61"
+  instance_type = "t2.micro"
+  key_name = "linuxserverrr"
+  tags = {
+    Name = "vamsi-vm"
+  }
+}
