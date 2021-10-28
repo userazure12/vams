91a9f5c9ff4cadb07a1eeba0a40106438a75788b
userazure12 <91203797+userazure12@users.noreply.github.com>
2021-10-28 17:39:00 +0530

    update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
   secret_key = "ceHbThHedNMJoLlShAIMdxE7kMoXBzbpz+G/wJmS"
 }
  
-resource "aws_instance" automatedproject-vm" {
+resource "aws_instance" automatedproject1-vm" {
   ami           = "ami-074cce78125f09d61"
   instance_type = "t2.micro"
   key_name = "linuxserverrr"
